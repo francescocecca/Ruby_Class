7fe1648e954e7d395c8fbdc37a944243ac1b5cd7
Francesco Pio Cecca <cecca.francesco@yahoo.it>
2023-11-24 17:06:18 +0100

    update

--- 6/new_rails_app/db/migrate/20231124153959_add_role_to_user.rb
+++ 6/new_rails_app/db/migrate/20231124153959_add_role_to_user.rb
@@ -1,4 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration[7.1]
   def change
+    add_reference :users, :roles
   end
 end
--- 6/new_rails_app/db/schema.rb
+++ 6/new_rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_150556) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_153959) do
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_150556) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "roles_id"
+    t.index ["roles_id"], name: "index_users_on_roles_id"
   end
 
 end
